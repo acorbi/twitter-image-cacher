be0cbcaf1076ded2cf458b5dc3083b0d362f125c
Open-Steps.org <contact@open-steps.org>
2014-10-23 18:11:14 +0200

    Obtaining now bigger image. Added cached/ with .gitkeep

--- twitter_profile_retriever.php
+++ twitter_profile_retriever.php
@@ -1,42 +1,46 @@
 <?php
-ini_set('display_errors', 1);
-require_once('TwitterAPIExchange.php');
-require_once('twitter_config.php');
 
-/** Get parameter */
-$screen_name = $_GET['screen_name'];
+	ini_set('display_errors', 1);
+	require_once('TwitterAPIExchange.php');
+	require_once('twitter_config.php');
 
-/** Before doing any request to the twitter API, look for cached url */
-$filename = "cached/".$screen_name;
+	/** Get parameter */
+	$screen_name = $_GET['screen_name'];
 
-/** If user's profile img was already chached, return it inmediatelly */
-if (file_exists($filename)){
+	/** Before doing any request to the twitter API, look for cached url */
+	$filename = "cached/".$screen_name;
 
-	echo file_get_contents($filename);
+	/** If user's profile img was already chached, return it inmediatelly */
+	if (file_exists($filename)){
 
-}else{
+		echo file_get_contents($filename);
 
-	/** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
-	$url = 'https://api.twitter.com/1.1/users/show.json';
-	$getfield = '?screen_name='.$screen_name;
-	$requestMethod = 'GET';
+	}else{
 
-	/** Perform a POST request and echo the response **/
-	$twitter = new TwitterAPIExchange($settings);
-	$response = $twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest();
+		/** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
+		$url = 'https://api.twitter.com/1.1/users/show.json';
+		$getfield = '?screen_name='.$screen_name;
+		$requestMethod = 'GET';
 
-	/** Decode JSON Object and extract/return parameter */
-	$json = json_decode($response, true);
+		/** Perform a POST request and echo the response **/
+		$twitter = new TwitterAPIExchange($settings);
+		$response = $twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest();
 
-	/** Store downloaded profile url in cached */
-	$cached[$screen_name] = $json["profile_image_url"];
+		/** Decode JSON Object and extract/return parameter */
+		$json = json_decode($response, true);
 
-	/** Dump contents of the array in the cached file again */
-	file_put_contents($filename,$json["profile_image_url"]);
+		/** Store downloaded profile url in cached */
+		$cached[$screen_name] = $json["profile_image_url"];
 
-	echo $json["profile_image_url"];
+		/** Remove _normal suffix to obtain bigger size **/
+		$json["profile_image_url"] = str_replace("_normal","",$json["profile_image_url"]);
 
-}
+		/** Dump contents of the array in the cached file again */
+		file_put_contents($filename,$json["profile_image_url"]);
 
+		header("Content-type: text/plain; charset=utf-8");
+		echo $json["profile_image_url"];
+
+	}
 
 ?>
