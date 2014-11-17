3a1a89a46289215c677988d049b983954dfc3622
Open-Steps.org <contact@open-steps.org>
2014-11-17 16:04:03 +0100

    little renaming

--- README.md
+++ README.md
@@ -7,8 +7,22 @@ PHP script returning URL for twitter's image of user specified by parameter. In 
 
 First you need to create a Twitter app under https://apps.twitter.com/
 
-1. Rename twitter_config_sample.php to twitter_config.php and specify Twitter APP credentials.
+1. Rename config.php.sample to config.php and specify Twitter APP credentials.
 2. Deploy PHP files on a webserver of your choice.
+
+# How to use
+
+Client has to GET-Request the twitter_profile_retriever.php file setting the **screen_name** parameter with the twitter handle, example:
+
+```
+GET http://server/twitter_profile_retriever.php?screen_name=acorbi
+```
+
+The script will return the URL of the twitter image in plain text:
+
+```
+http://pbs.twimg.com/profile_images/1113334395/Screen_shot_2010-08-26_at_1.48.20_AM_normal.png
+```
 
 # Used in
 
--- twitter_profile_retriever.php
+++ twitter_profile_retriever.php
@@ -1,46 +1,46 @@
 <?php
 
-	ini_set('display_errors', 1);
-	require_once('TwitterAPIExchange.php');
-	require_once('twitter_config.php');
+  ini_set('display_errors', 1);
+  require_once('TwitterAPIExchange.php');
+  require_once('config.php');
 
-	/** Get parameter */
-	$screen_name = $_GET['screen_name'];
+  /** Get parameter */
+  $screen_name = $_GET['screen_name'];
 
-	/** Before doing any request to the twitter API, look for cached url */
-	$filename = "cached/".$screen_name;
+  /** Before doing any request to the twitter API, look for cached url */
+  $filename = "cached/".$screen_name;
 
-	/** If user's profile img was already chached, return it inmediatelly */
-	if (file_exists($filename)){
+  /** If user's profile img was already chached, return it inmediatelly */
+  if (file_exists($filename)){
 
-		echo file_get_contents($filename);
+    echo file_get_contents($filename);
 
-	}else{
+  }else{
 
-		/** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
-		$url = 'https://api.twitter.com/1.1/users/show.json';
-		$getfield = '?screen_name='.$screen_name;
-		$requestMethod = 'GET';
+    /** URL for REST request, see: https://dev.twitter.com/docs/api/1.1/ **/
+    $url = 'https://api.twitter.com/1.1/users/show.json';
+    $getfield = '?screen_name='.$screen_name;
+    $requestMethod = 'GET';
 
-		/** Perform a POST request and echo the response **/
-		$twitter = new TwitterAPIExchange($settings);
-		$response = $twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest();
+    /** Perform a POST request and echo the response **/
+    $twitter = new TwitterAPIExchange($settings);
+    $response = $twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest();
 
-		/** Decode JSON Object and extract/return parameter */
-		$json = json_decode($response, true);
+    /** Decode JSON Object and extract/return parameter */
+    $json = json_decode($response, true);
 
-		/** Store downloaded profile url in cached */
-		$cached[$screen_name] = $json["profile_image_url"];
+    /** Store downloaded profile url in cached */
+    $cached[$screen_name] = $json["profile_image_url"];
 
-		/** Remove _normal suffix to obtain bigger size **/
-		$json["profile_image_url"] = str_replace("_normal","",$json["profile_image_url"]);
+    /** Remove _normal suffix to obtain bigger size **/
+    $json["profile_image_url"] = str_replace("_normal","",$json["profile_image_url"]);
 
-		/** Dump contents of the array in the cached file again */
-		file_put_contents($filename,$json["profile_image_url"]);
+    /** Dump contents of the array in the cached file again */
+    file_put_contents($filename,$json["profile_image_url"]);
 
-		header("Content-type: text/plain; charset=utf-8");
-		echo $json["profile_image_url"];
+    header("Content-type: text/plain; charset=utf-8");
+    echo $json["profile_image_url"];
 
-	}
+  }
 
 ?>
